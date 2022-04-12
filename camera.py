23a05eb97d5111b144c81767de84077c34f2e0e6
bjones117 <103345806+bjones117@users.noreply.github.com>
2022-04-12 19:27:33 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -13,7 +13,6 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
-    # returns camera frames along with bounding boxes and predictions
     def get_frame(self):
         _, fr = self.video.read()
         gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
@@ -22,8 +21,8 @@ class VideoCamera(object):
         for (x, y, w, h) in faces:
             fc = gray_fr[y:y+h, x:x+w]
 
-            roi = cv2.resize(fc, (48, 48))
-            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+            resizer = cv2.resize(fc, (48, 48))
+            pred = model.predict_emotion(resizer[np.newaxis, :, :, np.newaxis])
 
             cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
             cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
